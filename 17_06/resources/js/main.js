b59a7e34fea959646265c801e5c5ba4ef5bfa085
Sarra Lyford <slyford@sierrawireless.com>
2017-07-11 17:29:54 -0700

    Upload AWS Search URL
    Change-Id: Id1d211fb22c273ccaa2d8e04eb8d849e38bbd1a6
    Resolves: LE-7492

--- 17_06/resources/js/main.js
+++ 17_06/resources/js/main.js
@@ -1,4 +1,4 @@
-var invoke_url = "https://uol13g6g8c.execute-api.us-west-2.amazonaws.com/prod"; // cloudsearch api endpoint
+var invoke_url = "https://sj3kp3pd4g.execute-api.us-west-2.amazonaws.com/prod"; // cloudsearch api endpoint
 var restrict_to_ctx = false; // restrict search to current context?
 
 
